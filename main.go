5fd9fa324d90832e2e54b7ab7a014188695a7bbb
codercooke <ejcook111@gmail.com>
2019-11-08 17:00:24 -0500

    Proxy dns requests to 1.1.1.1

--- main.go
+++ main.go
@@ -8,49 +8,55 @@ import (
 
 func main() {
 	log.Print("Starting")
-
-	l, err := net.ListenUDP("udp", &net.UDPAddr{Port: 53})
+	origin := &net.UDPAddr{Port: 1053}
+	l, err := net.ListenUDP("udp", origin)
 	if err != nil {
 		log.Fatal("listen error:", err)
 	}
 	defer l.Close()
+	message := map[uint16]*net.UDPAddr{}
 
 	for {
 		buf := make([]byte, 512)
 		_, addr, err := l.ReadFromUDP(buf)
-		go func() {
-			if err != nil {
-				log.Fatal("accept error:", err)
-			}
-			var m dnsmessage.Message
-			err = m.Unpack(buf)
-			if err != nil {
-				log.Fatal("unpack err:", err)
-			}
 
-			// question := m.Questions[0]
-			// log.Printf("%v", question.Name.String())
-			// if strings.Contains(question.Name.String(), "facebook") {
-			// 	println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& facebook")
-			// }
+		if err != nil {
+			log.Fatal("accept error:", err)
+		}
 
-			packed, err := m.Pack()
+		var m dnsmessage.Message
+		err = m.Unpack(buf)
+		if err != nil {
+			log.Fatal("unpack err:", err)
+		}
+
+		log.Printf("%s", m)
+		log.Printf("%s", m.Header)
+		log.Printf("%s", m.Header.Response)
+
+		packed, err := m.Pack()
+
+		if m.Header.Response {
+			println("MADE IT HERE")
+			log.Printf("%s", message[m.Header.ID])
+			i, err := l.WriteToUDP(packed, message[m.Header.ID])
+			delete(message, m.Header.ID)
+			println(i)
 			if err != nil {
-				log.Fatal("Pack err:", err)
+				log.Fatal("Write err:", err)
 			}
+			if len(m.Answers) > 0 {
+				log.Printf("Questions: %v, type: %v, Answers: %v", m.Questions, m.Answers[0].Header.GoString(), m.Answers[0].Body.GoString())
+			}
+		} else {
+			message[m.Header.ID] = addr
 
-			log.Printf("here")
 			resolver := net.UDPAddr{IP: net.IP{1, 1, 1, 1}, Port: 53}
 			_, err = l.WriteToUDP(packed, &resolver)
-
-			if m.Header.Response {
-				log.Printf("Questions: %v, Answers:", m.Questions, m.Answers[0].Body.GoString())
-				packed, err = m.Pack()
-				_, err = l.WriteToUDP(packed, addr)
-				if err != nil {
-					log.Fatal("Write err:", err)
-				}
+			if err != nil {
+				log.Fatal("failed to resolve", err)
 			}
-		}()
+		}
 	}
+	println("exiting")
 }
