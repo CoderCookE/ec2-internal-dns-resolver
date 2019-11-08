a4f6795775233cfa5bcce209f8e2a27548f56e1f
codercooke <ejcook111@gmail.com>
2019-11-08 18:18:47 -0500

    resolve aws ec internal dns

--- main.go
+++ main.go
@@ -4,15 +4,19 @@ import (
 	"golang.org/x/net/dns/dnsmessage"
 	"log"
 	"net"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	log.Print("Starting")
-	origin := &net.UDPAddr{Port: 1053}
+	origin := &net.UDPAddr{Port: 53}
 	l, err := net.ListenUDP("udp", origin)
 	if err != nil {
 		log.Fatal("listen error:", err)
 	}
+
 	defer l.Close()
 	message := map[uint16]*net.UDPAddr{}
 
@@ -30,20 +34,55 @@ func main() {
 			log.Fatal("unpack err:", err)
 		}
 
-		log.Printf("%s", m)
-		log.Printf("%s", m.Header)
-		log.Printf("%s", m.Header.Response)
+		question := m.Questions[0]
+		log.Printf("%v", question.Name.String())
+
+		if strings.Contains(question.Name.String(), "ec2.internal") {
+			message[m.Header.ID] = addr
+
+			re := regexp.MustCompile("(\\d{1,3})-(\\d{1,3})-(\\d{1,3})-(\\d{1,3})")
+			log.Printf("found ec address")
+			toCheck := []byte(question.Name.String())
+			matches := re.FindSubmatch(toCheck)
+
+			log.Printf("%s", matches)
+
+			resolved := [4]byte{}
+			for i, v := range matches[1:] {
+				str := string(v)
+				println("string", str)
+				val, _ := strconv.Atoi(str)
+				println("integer", val)
+				resolved[i] = uint8(val)
+			}
+
+			println(m.Questions)
+			m.Header.Response = true
+			m.Header.Authoritative = true
+			newAnswers := []dnsmessage.Resource{
+				{
+					Header: dnsmessage.ResourceHeader{
+						Name:   dnsmessage.MustNewName(m.Questions[0].Name.String()),
+						Type:   dnsmessage.TypeA,
+						Class:  dnsmessage.ClassINET,
+						TTL:    278,
+						Length: 22,
+					},
+
+					Body: &dnsmessage.AResource{A: resolved},
+				},
+			}
+
+			m.Answers = newAnswers
+		}
 
 		packed, err := m.Pack()
 
 		if m.Header.Response {
-			println("MADE IT HERE")
 			log.Printf("%s", message[m.Header.ID])
-			i, err := l.WriteToUDP(packed, message[m.Header.ID])
-			delete(message, m.Header.ID)
-			println(i)
+			_, err := l.WriteToUDP(packed, message[m.Header.ID])
 			if err != nil {
-				log.Fatal("Write err:", err)
+				log.Printf("Write err: %s", err)
 			}
 			if len(m.Answers) > 0 {
 				log.Printf("Questions: %v, type: %v, Answers: %v", m.Questions, m.Answers[0].Header.GoString(), m.Answers[0].Body.GoString())
@@ -54,9 +93,10 @@ func main() {
 			resolver := net.UDPAddr{IP: net.IP{1, 1, 1, 1}, Port: 53}
 			_, err = l.WriteToUDP(packed, &resolver)
 			if err != nil {
-				log.Fatal("failed to resolve", err)
+				log.Printf("failed to resolve %s", err)
 			}
 		}
 	}
+
 	println("exiting")
 }
